976eafc6e659782f9ddd0f69b757e58e00121353
Andrew Hong <andrew.jwn.hong@gmail.com>
2024-05-29 21:16:06 -0400

    Very crude version of Shurl

--- shurl/app.py
+++ shurl/app.py
@@ -1,11 +1,43 @@
+from typing import List
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from collections import Counter
+import itertools
+
+
 class App:
     def __init__(self) -> None:
-        pass
+        self.tfidf = TfidfVectorizer()
+        nltk.download("punkt")
+        nltk.download("stopwords")
 
-    def run(self) -> None:
-        print("This is boilerplate!")
+    def run(self, text: str, options_count: int = 5) -> List[str]:
+        # text = preprocess(text)
+        text = text.lower()
+
+        stop_words = set(stopwords.words("english"))
+        words = word_tokenize(text)
+        filtered_words = [
+            word for word in words if word.isalnum() and word not in stop_words
+        ]
+
+        common_words = Counter(filtered_words).most_common(15)
+        keywords = [word for word, _ in common_words]
+
+        options = []
+        for length in range(1, min(5, len(keywords)) + 1):
+            for combination in itertools.combinations(keywords, length):
+                option = "-".join(combination)
+                options.append(option)
+
+        return options[:options_count]
 
 
 if __name__ == "__main__":
     app = App()
-    app.run()
+    app.run(
+        """The upcoming fields are pretty standard SSH
+information , nothing specific for MongoDB."""
+    )
