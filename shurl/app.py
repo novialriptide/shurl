e42892c8247c6a44f6bf8544a41943545b4d61ba
Andrew Hong <andrew.jwn.hong@gmail.com>
2024-06-05 14:24:58 -0400

    app.py cleanup

--- shurl/app.py
+++ shurl/app.py
@@ -1,15 +1,17 @@
+from typing import Any
 from utils import parse_training_data_from_bson
 from config import CACHE_DIRECTORY
-from typing import List
 import os
 import json
 
 
 class App:
-    def __init__(self) -> None:
-        self.execute_cache()
+    def __init__(self, epoch_count: int, batch_size: int) -> None:
+        self.load_data()
+        self.epoch_count = epoch_count
+        self.batch_size = batch_size
 
-    def execute_cache(self) -> None:
+    def load_data(self) -> Any:
         cache_file_path = os.path.join(CACHE_DIRECTORY, "data.json")
 
         if os.path.isfile(cache_file_path):
@@ -23,13 +25,8 @@ class App:
         with open(cache_file_path, "w+") as file:
             json.dump(data, file, indent=4)
 
-    def run(self, text: str, options_count: int = 5) -> List[str]:
-        return []
+        return data
 
 
 if __name__ == "__main__":
-    app = App()
-    app.run(
-        """The upcoming fields are pretty standard SSH
-information , nothing specific for MongoDB."""
-    )
+    app = App(epoch_count=100, batch_size=64)
