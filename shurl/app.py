ff39a7a70ba930e047cdbaba24b4ea36f94c3f84
Andrew Hong <andrew.jwn.hong@gmail.com>
2024-06-06 01:54:37 -0400

    Move initialization for Shurl app outside the if main

--- shurl/app.py
+++ shurl/app.py
@@ -2,6 +2,7 @@ from shurl import Shurl  # type: ignore
 from flask import Flask
 
 app = Flask(__name__)
+shurl_app = Shurl(epoch_count=100, batch_size=64)
 
 
 @app.route("/")
@@ -10,5 +11,4 @@ def hello_world() -> str:
 
 
 if __name__ == "__main__":
-    shurl_app = Shurl(epoch_count=100, batch_size=64)
     app.run(host="0.0.0.0", port=4352, debug=True)
